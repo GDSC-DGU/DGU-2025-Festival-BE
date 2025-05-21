cf116f51ddb4889c4109998aeab69edc9ad3a2d9
ansungwoo <asw4ever@naver.com>
2025-05-22 01:24:49 +0900

    [Chore] #1 오타 수정

--- src/main/java/gdg/festa/presentation/controller/ReserveController.java
+++ src/main/java/gdg/festa/presentation/controller/ReserveController.java
@@ -1,8 +1,8 @@
 package gdg.festa.presentation.controller;
 
 import gdg.festa.application.usecase.reserve.CreateReserveUseCase;
-import gdg.festa.application.usecase.reserve.ReadReverseUsecase;
-import gdg.festa.application.usecase.reserve.UpdateReverseUsecase;
+import gdg.festa.application.usecase.reserve.ReadReserveUsecase;
+import gdg.festa.application.usecase.reserve.UpdateReserveUsecase;
 import gdg.festa.core.common.CommonResponseDto;
 import gdg.festa.presentation.request.reserve.CreateReserveRequestDto;
 import lombok.RequiredArgsConstructor;
@@ -12,22 +12,22 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/reserve")
 @RequiredArgsConstructor
 public class ReserveController {
-    private final ReadReverseUsecase readReverseUsecase;
-    private final UpdateReverseUsecase updateReverseUsecase;
+    private final ReadReserveUsecase readReserveUsecase;
+    private final UpdateReserveUsecase updateReserveUsecase;
     private final CreateReserveUseCase createReserveUseCase;
 
     @GetMapping("")
     public CommonResponseDto<?> findreserve(
             @RequestParam String number
     ){
-        return CommonResponseDto.ok(readReverseUsecase.execute(number));
+        return CommonResponseDto.ok(readReserveUsecase.execute(number));
     }
 
     @PatchMapping("")
     public CommonResponseDto<?> cancelReserve(
             @RequestParam String number
     ){
-        return CommonResponseDto.ok(updateReverseUsecase.execute(number));
+        return CommonResponseDto.ok(updateReserveUsecase.execute(number));
     }
 
     @PostMapping("/{boothsId}")
