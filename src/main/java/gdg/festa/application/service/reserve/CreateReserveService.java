ff8677fc9ddbacbeb5035a995cacace9f1c84f27
park20011029 <rokmck156@gmail.com>
2025-05-26 12:56:48 +0900

    Fix: 예약관리에서의 예외처리 강화

--- src/main/java/gdg/festa/application/service/reserve/CreateReserveService.java
+++ src/main/java/gdg/festa/application/service/reserve/CreateReserveService.java
@@ -23,10 +23,10 @@ public class CreateReserveService implements CreateReserveUseCase {
     public Boolean execute(Long boothId, CreateReserveRequestDto createReserveRequestDto) {
         Pub pub = pubRepository.findById(boothId);
         if(pub.getPubStatus().name().equals("AVAILABLE"))
-            throw new CustomException(ErrorCode.SERVER_ERROR); // 예약없이 입장이 가능합니다.
+            throw new CustomException(ErrorCode.ACCESS_AVAILABLE); // 예약없이 입장이 가능합니다.
 
         if(pub.getPubStatus().name().equals("END") || pub.getPubStatus().name().equals("PREPARING"))
-            throw new CustomException(ErrorCode.SERVER_ERROR); // 부스가 운영을 중단하였습니다.
+            throw new CustomException(ErrorCode.ACCESS_STOP); // 부스가 운영을 중단하였습니다.
 
         if(reserveRepository.existsByPhoneNumberAndReserveStatus(createReserveRequestDto.phoneNumber(), ReserveStatus.WAITING))
             throw new CustomException(ErrorCode.CONFLICT_RESERVE);
