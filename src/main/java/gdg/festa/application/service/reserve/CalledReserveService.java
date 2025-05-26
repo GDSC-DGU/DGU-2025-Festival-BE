20d2d8c9801ec552b48ec0d5069b779ee36f51fe
ansungwoo <asw4ever@naver.com>
2025-05-27 05:10:57 +0900

    [Fix] 스케줄러 10분 -> 3분 수정 및 END 으로 바뀌는 경우, 대기 인원들 대기 정보 CANCELED로 수정

--- src/main/java/gdg/festa/application/service/reserve/CalledReserveService.java
+++ src/main/java/gdg/festa/application/service/reserve/CalledReserveService.java
@@ -34,7 +34,7 @@ public class CalledReserveService implements CalledReserveUseCase {
 
         reserve.updateStatus(ReserveStatus.CALLED);
 
-        String message = reserve.getPub().getName() + " 주점 입장 가능 알림 : 지금 입장하실 수 있습니다. 10분 이내로 입장해 주세요.";
+        String message = reserve.getPub().getName() + " 주점 입장 가능 알림 : 지금 입장하실 수 있습니다. 3분 이내로 입장해 주세요.";
         smsUtil.sendMessage(reserve.getPhoneNumber(), message);
 
 //        fcmUtil.sendMessage(
