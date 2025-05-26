6073cc3347b0b3c0612335d95d27b23d17864862
park20011029 <rokmck156@gmail.com>
2025-05-27 04:14:50 +0900

    Fix: fcm -> sms 변경

--- src/main/java/gdg/festa/application/dto/pub/ReadPubWaitingUserListResponseDto.java
+++ src/main/java/gdg/festa/application/dto/pub/ReadPubWaitingUserListResponseDto.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @Builder
 public record ReadPubWaitingUserListResponseDto(
-
+        String pubStatus,
         Long waitingTotalCount, // 전체 현재 대기 팀 수
         Long lateTotalCount, // 전체 시간 경과 대기 팀 수
         List<ReserveInfo> reserveList // 대기 팀 리스트
